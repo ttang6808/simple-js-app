15ac7fcbcd32952e32a346cf6fc7abb3d22bd59d
ttang6808 <91977113+ttang6808@users.noreply.github.com>
2021-10-18 14:21:26 -0400

    Updated script.js with pokemon objects
    Including properties name, height, and type

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-alert('Hello world');
+let pokemonList = {};
 
-let favoriteFood = 'kimbap';
-document.write(favoriteFood);
+//Height is in meters
+pokemonList[0] = {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']};
+pokemonList[1] = {name: 'Ninetales', height: 1.1, types: 'fire'};
+pokemonList[2] = {name: 'Lickitung', height: 1.2, types: 'normal'};
+pokemonList[3] = {name: 'Espeon', height: 0.9, types: 'psychic'};
