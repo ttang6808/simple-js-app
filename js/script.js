e3794779d639028300684652bd89cce42fbb1e2c
ttang6808 <91977113+ttang6808@users.noreply.github.com>
2021-10-18 18:52:14 -0400

    Corrected pokemonList variable objects

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let pokemonList = {};
-
+let pokemonList = [
+  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+  {name: 'Ninetales', height: 1.1, types: ['fire']},
+  {name: 'Lickitung', height: 1.2, types: ['normal']},
+  {name: 'Espeon', height: 0.9, types: ['psychic']}
+];
 //Height is in meters
-pokemonList[0] = {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']};
-pokemonList[1] = {name: 'Ninetales', height: 1.1, types: 'fire'};
-pokemonList[2] = {name: 'Lickitung', height: 1.2, types: 'normal'};
-pokemonList[3] = {name: 'Espeon', height: 0.9, types: 'psychic'};
