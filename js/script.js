f87b1bfccfbfeabc9f0d3253a6fa8c42cdb4c546
ttang6808 <91977113+ttang6808@users.noreply.github.com>
2021-10-20 14:18:03 -0400

    Created for loop to find maximum height

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 let pokemonList = [
-  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-  {name: 'Ninetales', height: 1.1, types: ['fire']},
-  {name: 'Lickitung', height: 1.2, types: ['normal']},
-  {name: 'Espeon', height: 0.9, types: ['psychic']}
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Ninetales',
+    height: 1.1,
+    types: ['fire']
+  },
+  {
+    name: 'Lickitung',
+    height: 1.2,
+    types: ['normal']
+  },
+  {
+    name: 'Espeon',
+    height: 0.9,
+    types: ['psychic']
+  }
 ];
-//Height is in meters
+
+// Iterating to find the maximum height value
+let maxHeight = pokemonList[0].height;
+for (let i = 0; i < pokemonList.length; i++){
+  if (pokemonList[i].height > largest){
+    maxHeight = pokemonList[i].height;
+  }
+}
