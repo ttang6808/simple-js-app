a16c1f4311a9649525000ef0e0556cadb0d64286
ttang6808 <91977113+ttang6808@users.noreply.github.com>
2021-12-02 14:34:53 -0500

    Created loadList function

--- js/script.js
+++ js/script.js
@@ -98,6 +98,22 @@ let pokemonRepository = (function(){
     console.log(pokemon.name);
   }
 
+  function loadList(){
+    return fetch(apiUrl).then(function (response){
+      return response.json();
+    }).return(function (json){
+      json.results.forEach(function (item){
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e){
+      console.error(e);
+    })
+  }
+
   return{
     add: add,
     getAll: getAll,
