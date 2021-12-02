65128152836ff66803d152743f95d63d19cbb955
ttang6808 <91977113+ttang6808@users.noreply.github.com>
2021-12-02 14:41:32 -0500

    Updated repository return object with assigned keys to loadList and loadDetails functions

--- js/script.js
+++ js/script.js
@@ -132,7 +132,9 @@ let pokemonRepository = (function(){
   return{
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 })();
 
