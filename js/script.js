8ffa65daae151a225397c7b85aa51ba2cb5d5be1
ttang6808 <91977113+ttang6808@users.noreply.github.com>
2021-12-02 15:44:09 -0500

    Cleaning up unused code

--- js/script.js
+++ js/script.js
@@ -1,62 +1,6 @@
 let pokemonRepository = (function(){
-  // let pokemonList = [
-  //   {
-  //     name: 'Bulbasaur',
-  //     height: 0.7,
-  //     types: ['grass', 'poison']
-  //   },
-  //   {
-  //     name: 'Ninetales',
-  //     height: 1.1,
-  //     types: ['fire']
-  //   },
-  //   {
-  //     name: 'Lickitung',
-  //     height: 1.2,
-  //     types: ['normal']
-  //   },
-  //   {
-  //     name: 'Espeon',
-  //     height: 0.9,
-  //     types: ['psychic']
-  //   }
-  // ];
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-
-  // isObject function to check if parameter is an object
-  function isObject(item){
-    if (typeof(item) === 'object'){
-      return true;
-    }
-    else{
-      alert('Add function parameter is not an object.')
-    }
-  }
-
-  // isObject function to check if parameter is an object
-  function isObject(item){
-    if (typeof(item) === 'object'){
-      return true;
-    }
-    else{
-      alert('Add function parameter is not an object.');
-    }
-  }
-
-  // compareKeys function to check if the keys between objects are equal
-  function compareKeys(object1, object2){
-    let key1 = Object.keys(object1);
-    let key2 = Object.keys(object2);
-    let key1String = key1.join();
-    let key2String = key2.join();
-    if (key1String === key2String){
-      return true;
-    }
-    else{
-      alert('Parameter keys are not the same.');
-    }
-  }
 
   // add function to input pokemon into empty pokemon array
   function add(pokemon){
